2068d106c6a30eda5cf817fd311c367275c1c9b7
Brian Fabella <fabellb@rockefeller.edu>
2026-01-14 17:36:02 -0500

    Added iterative_design pics and movie.

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -3,24 +3,42 @@
     // Create modal element
     var $modal = $('<div id="zoom-modal"></div>');
     var $modalImg = $('<img>');
+    var $modalVideo = $('<video controls autoplay style="display:none; max-width: 90%; max-height: 90%;"></video>');
     var $closeBtn = $('<span class="close">&times;</span>');
 
     $modal.append($closeBtn);
     $modal.append($modalImg);
+    $modal.append($modalVideo);
     $('body').append($modal);
 
     // Open modal on image click
     $('.zoom').on('click', function() {
-      var src = $(this).attr('src');
-      $modalImg.attr('src', src);
-      $modal.addClass('active');
+      var videoSrc = $(this).attr('data-video');
+      
+      if (videoSrc) {
+        $modalImg.hide();
+        $modalVideo.attr('src', videoSrc).show();
+        $modal.addClass('active');
+        // Play video after modal is shown to ensure it works
+        $modalVideo[0].play();
+      } else {
+        var src = $(this).attr('src');
+        $modalVideo.hide();
+        $modalVideo[0].pause();
+        $modalVideo.attr('src', '');
+        $modalImg.attr('src', src).show();
+        $modal.addClass('active');
+      }
     });
 
     // Close modal on click outside or close button
     $modal.on('click', function(e) {
-      if (e.target !== $modalImg[0]) {
+      if (e.target !== $modalImg[0] && e.target !== $modalVideo[0]) {
         $modal.removeClass('active');
         $modalImg.attr('src', '');
+        $modalVideo[0].pause();
+        $modalVideo.attr('src', '');
+        $modalVideo.hide(); // Hide video on close
       }
     });
   });
